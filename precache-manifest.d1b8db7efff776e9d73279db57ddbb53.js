66c38dd445b867de2388ebcaa3e5b5001f027f16
eugene <lasttrip@ukr.net>
2020-10-14 16:01:22 +0300

    Updates

---
+++ precache-manifest.d1b8db7efff776e9d73279db57ddbb53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54a3a40afead1c1228147ed8307d16e",
+    "url": "/millionaire-game/index.html"
+  },
+  {
+    "revision": "811578f47e619ffd14c4",
+    "url": "/millionaire-game/static/css/main.422ed981.chunk.css"
+  },
+  {
+    "revision": "2a98e8ba8c3866fdcaf0",
+    "url": "/millionaire-game/static/js/2.114343a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/millionaire-game/static/js/2.114343a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811578f47e619ffd14c4",
+    "url": "/millionaire-game/static/js/main.50729b75.chunk.js"
+  },
+  {
+    "revision": "2a033830dfbcc658386f",
+    "url": "/millionaire-game/static/js/runtime-main.4d11fc25.js"
+  },
+  {
+    "revision": "f8c75481696354bfdabed0ba328d3476",
+    "url": "/millionaire-game/static/media/hand.f8c75481.png"
+  }
+]);
