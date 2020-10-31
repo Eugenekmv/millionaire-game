a5c280157e61e910523ab5f3f42a9fec36bb56e0
eugene <lasttrip@ukr.net>
2020-10-31 10:00:17 +0200

    initial with ts

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,85 +1,89 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
-import gameJson from '../../static/game.json';
+import gameJson from '../../static/game.json'
 
-import NavState from '../../context/navContext';
-import AnswerList from './AnswerList';
-import BurgerButton from '../UI/BurgerButton';
-import Stages from './Stages';
+import NavState from '../../context/navContext'
+import AnswerList from './AnswerList'
+import BurgerButton from '../UI/BurgerButton'
+import Stages from './Stages'
 
-import './style.css';
+import './style.css'
 
 const Question = () => {
-  const [activeQuestion, setActiveQuestion] = useState(0);
-  const [answeredQuestion, setAnsweredQuestion] = useState(null);
+    const [activeQuestion, setActiveQuestion] = useState(0)
+    const [answeredQuestion, setAnsweredQuestion] = useState(null)
 
-  const history = useHistory();
+    const history = useHistory()
 
-  const { quiz } = gameJson;
-  const { stages } = gameJson;
-  const { answers } = gameJson.quiz[activeQuestion];
-  const question = gameJson.quiz[activeQuestion];
+    const { quiz } = gameJson
+    const { stages } = gameJson
+    const { answers } = gameJson.quiz[activeQuestion]
+    const question = gameJson.quiz[activeQuestion]
 
-  const onAnswerClick = (answerId) => {
-    if (answerId === question.rightAnswerId) {
-      setAnsweredQuestion({ [answerId]: 'success' });
-
-      const isGameFinished = () => {
-        return activeQuestion + 1 === quiz.length;
-      };
-
-      const timeout = setTimeout(() => {
-        if (isGameFinished()) {
-          history.push({
-            pathname: '/millionaire-game/finish',
-            state: {
-              prizeStep: activeQuestion,
-            },
-          });
-          setAnsweredQuestion(null);
-          setActiveQuestion(0);
-        } else {
-          setActiveQuestion((prev) => prev + 1);
-          setAnsweredQuestion(null);
+    const onAnswerClick = (answerId) => {
+        if (answeredQuestion) {
+            return
         }
+        if (answerId === question.rightAnswerId) {
+            setAnsweredQuestion({ [answerId]: 'success' })
 
-        clearTimeout(timeout);
-      }, 1000);
-    } else {
-      setAnsweredQuestion({ [answerId]: 'failed' });
-      const timeout = setTimeout(() => {
-        history.push({
-          pathname: '/millionaire-game/finish',
-          state: {
-            prizeStep: activeQuestion - 1,
-          },
-        });
-        clearTimeout(timeout);
-      }, 1000);
+            const isGameFinished = () => {
+                return activeQuestion + 1 === quiz.length
+            }
+
+            const timeout = setTimeout(() => {
+                if (isGameFinished()) {
+                    history.push({
+                        pathname: '/millionaire-game/finish',
+                        state: {
+                            prizeStep: activeQuestion,
+                        },
+                    })
+                    setAnsweredQuestion(null)
+                    setActiveQuestion(0)
+                } else {
+                    setActiveQuestion((prev) => prev + 1)
+                    setAnsweredQuestion(null)
+                }
+
+                clearTimeout(timeout)
+            }, 1000)
+        } else {
+            setAnsweredQuestion({ [answerId]: 'failed' })
+            const timeout = setTimeout(() => {
+                history.push({
+                    pathname: '/millionaire-game/finish',
+                    state: {
+                        prizeStep: activeQuestion - 1,
+                    },
+                })
+                clearTimeout(timeout)
+            }, 1000)
+        }
     }
-  };
 
-  return (
-    <NavState>
-      <div className="quiz-container">
-        <div className="quiz-game">
-          <div className="game-container">
-            <span className="quiz-game__title">{question.question}</span>
-            <div className="quiz-game__questions">
-              <AnswerList
-                answers={answers}
-                onAnswerClick={onAnswerClick}
-                answeredQuestion={answeredQuestion}
-              />
+    return (
+        <NavState>
+            <div className="quiz-container">
+                <div className="quiz-game">
+                    <div className="game-container">
+                        <span className="quiz-game__title">{question.question}</span>
+                        <div className="quiz-game__questions">
+                            <AnswerList
+                                answers={answers}
+                                onAnswerClick={onAnswerClick}
+                                answeredQuestion={answeredQuestion}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <BurgerButton />
+
+                <Stages stages={stages} activeQuestion={activeQuestion} />
             </div>
-          </div>
-        </div>
-        <BurgerButton />
-        <Stages stages={stages} activeQuestion={activeQuestion} />
-      </div>
-    </NavState>
-  );
-};
+        </NavState>
+    )
+}
 
-export default Question;
+export default Question
